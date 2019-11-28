8b7c804802276e076a512c8bf2fed780d0978dbf
Anirudh Sharma <anirudh03sharma@gmail.com>
2019-11-28 20:58:09 +0530

    Updated the priority queue code for the comparable

--- PrepWork/src/main/java/org/redquark/ramanujan/prepwork/ds/PriorityQueue.java
+++ PrepWork/src/main/java/org/redquark/ramanujan/prepwork/ds/PriorityQueue.java
@@ -1,7 +1,6 @@
 package org.redquark.ramanujan.prepwork.ds;
 
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -9,7 +8,7 @@ import java.util.NoSuchElementException;
  * @author Anirudh Sharma
  *
  */
-public class PriorityQueue<T> {
+public class PriorityQueue<T extends Comparable<T>> {
 
 	// Default capacity of the PriorityQueue
 	private static final int DEFAULT_CAPACITY = 100;
@@ -17,8 +16,6 @@ public class PriorityQueue<T> {
 	private T[] heap;
 	// Number of elements in the heap
 	private int currentSize;
-	// Underlying comparator
-	private Comparator<? super T> comparator;
 
 	/**
 	 * Default constructor that creates an empty priority queue
@@ -26,17 +23,6 @@ public class PriorityQueue<T> {
 	@SuppressWarnings("unchecked")
 	public PriorityQueue() {
 		this.currentSize = 0;
-		this.comparator = null;
-		this.heap = (T[]) new Object[DEFAULT_CAPACITY + 1];
-	}
-
-	/**
-	 * Constructor that creates a priority queue with the specified comparator
-	 */
-	@SuppressWarnings("unchecked")
-	public PriorityQueue(Comparator<? super T> comparator) {
-		this.currentSize = 0;
-		this.comparator = comparator;
 		this.heap = (T[]) new Object[DEFAULT_CAPACITY + 1];
 	}
 
@@ -45,7 +31,6 @@ public class PriorityQueue<T> {
 	 */
 	@SuppressWarnings("unchecked")
 	public PriorityQueue(Collection<? extends T> collection) {
-		this.comparator = null;
 		this.currentSize = collection.size();
 		heap = (T[]) new Object[(currentSize + 2) * 11 / 10];
 
@@ -71,7 +56,7 @@ public class PriorityQueue<T> {
 		int hole = ++currentSize;
 		heap[0] = item;
 
-		for (; compare(item, heap[hole / 2]) < 0; hole /= 2) {
+		for (; item.compareTo(heap[hole / 2]) < 0; hole /= 2) {
 			heap[hole] = heap[hole / 2];
 		}
 		heap[hole] = item;
@@ -152,28 +137,16 @@ public class PriorityQueue<T> {
 		T temp = heap[hole];
 		for (; hole * 2 <= currentSize; hole = child) {
 			child = hole * 2;
-			if (child != currentSize && compare(heap[child + 1], heap[child]) < 0) {
+			if (child != currentSize && heap[child + 1].compareTo(heap[child]) < 0) {
 				child++;
 			}
-			if (compare(heap[child], temp) < 0) {
+			if (heap[child].compareTo(temp) < 0) {
 				heap[hole] = heap[child];
 			} else {
 				break;
 			}
 		}
 		heap[hole] = temp;
-	}
-
-	/**
-	 * Compares lhs and rhs using comparator if provided, or the default comparator.
-	 */
-	@SuppressWarnings("unchecked")
-	private int compare(T lhs, T rhs) {
-		if (comparator == null) {
-			return ((Comparable<T>) lhs).compareTo(rhs);
-		} else {
-			return comparator.compare(lhs, rhs);
-		}
 	}
 
 	/**
