58dee38c314ca152b8f2f1b118458dbc4467931b
Anirudh Sharma <anirudh03sharma@gmail.com>
2020-02-20 21:53:28 +0530

    Added code for merging two arrays

---
+++ Problem-Solving/src/main/java/org/redquark/ramanujan/ps/arrays/Arrays011_MergeTwoArrays.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.redquark.ramanujan.ps.arrays;
+
+/**
+ * @author Anirudh Sharma
+ *
+ */
+public class Arrays011_MergeTwoArrays {
+
+	/**
+	 * This method moves vacant cells in the beginning of the array and then merge
+	 * it with the smaller array
+	 */
+	public int[] mergeSorted(int[] a, int[] b) {
+		// Moves non empty elements of a in the beginning
+		int k = 0;
+		// Loop for all the elements in the array
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] != 0) {
+				a[k++] = a[i];
+			}
+		}
+		return merge(a, b, k - 1, b.length - 1);
+	}
+
+	private int[] merge(int[] a, int[] b, int m, int n) {
+		// Size of a[] is k+1
+		int k = m + n + 1;
+		// Run till a[] or if b[] has elements left
+		while (m >= 0 && n >= 0) {
+			// Put next greater elements in next free position in a[] from end
+			if (a[m] > b[n]) {
+				a[k--] = a[m--];
+			} else {
+				a[k--] = b[n--];
+			}
+		}
+		// Copy remaining elements of b[] (if any) in a[]
+		while (n >= 0) {
+			a[k--] = b[n--];
+		}
+		return a;
+	}
+}
---
+++ Problem-Solving/src/test/java/org/redquark/ramanujan/ps/arrays/Arrays011_MergeTwoArraysTest.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.redquark.ramanujan.ps.arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author aniru
+ *
+ */
+class Arrays011_MergeTwoArraysTest {
+
+	private Arrays011_MergeTwoArrays mergeArrays = new Arrays011_MergeTwoArrays();
+
+	/**
+	 * Test method for
+	 * {@link org.redquark.ramanujan.ps.arrays.Arrays011_MergeTwoArrays#mergeSorted(int[], int[])}.
+	 */
+	@Test
+	void testMergeSorted() {
+		// Vacant cells in X[] is represented by 0
+		int a[] = { 0, 2, 0, 3, 0, 5, 6, 0, 0 };
+		int b[] = { 1, 8, 9, 10, 15 };
+		int[] expected = { 1, 2, 3, 5, 6, 8, 9, 10, 15 };
+		assertArrayEquals(expected, mergeArrays.mergeSorted(a, b));
+	}
+
+}
